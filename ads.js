85cabf1528fda8a86cc238e14f0bd268a12338c3
vortex-media-network <sukhedhaban5@gmail.com>
2026-09-01 15:43:22 +0530

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,7 @@
+const AD_CONFIG = {
+  // Your Active Adsterra Smartlink
+  directLink: "https://www.profitableratecpmnetwork.com/bwk3i6y31?key=6e1b96d31c0b3e0dcc81cd10c035d3dd",
+
+  // Open Adsterra link on order submit (true / false)
+  openAdOnOrder: true
+};
